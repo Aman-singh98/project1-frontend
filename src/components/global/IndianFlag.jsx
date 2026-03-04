3b7417f20971b423135ea050e6f81136eca9f02d
Aman Singh <amansingh160798@gmail.com>
2026-03-05 00:36:42 +0530

    add indian flag

--- src/components/global/IndianFlag.jsx
+++ src/components/global/IndianFlag.jsx
@@ -4,17 +4,17 @@ function IndianFlag() {
     const slices = Array.from({ length: 50 });
 
     return (
-        <div className="flag">
-      {slices.map((_, i) => (
-        <span
-          key={i}
-          style={{
-            animationDelay: `${i * 0.04}s`,
-            backgroundPosition: `${-i * 2}px 0`
-          }}
-        />
-      ))}
-    </div>
+            <div className="flag">
+          {slices.map((_, i) => (
+            <span
+              key={i}
+              style={{
+                animationDelay: `${i * 0.04}s`,
+                backgroundPosition: `${-i * 2}px 0`
+              }}
+            />
+          ))}
+        </div>
     );
 }
 
